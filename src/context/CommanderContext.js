73265fa0b2ebee213f83d63df93db5d6219bbd51
Roberto Marungo <robmarungo.dev@gmail.com>
2023-07-03 01:59:48 -0500

    Updated Commander Context

--- src/context/CommanderContext.js
+++ src/context/CommanderContext.js
@@ -3,6 +3,10 @@ import { createContext, useState, useEffect } from 'react';
 // Create Commander Context
 const CommanderContext = createContext();
 
-export const CommanderProvider = ({ children }) => {};
+export const CommanderProvider = ({ children }) => {
+  // State
+  const [isLoading, setIsLoading] = useState(true);
+  const [cardData, setCardData] = useState([]);
+};
 
 export default CommanderContext;
