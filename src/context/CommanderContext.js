33080292d97c94cdcf6dd0d66b0abb38307e6b0f
Roberto Marungo <robmarungo.dev@gmail.com>
2023-07-03 02:01:39 -0500

    Updated Commander Context

--- src/context/CommanderContext.js
+++ src/context/CommanderContext.js
@@ -7,6 +7,10 @@ export const CommanderProvider = ({ children }) => {
   // State
   const [isLoading, setIsLoading] = useState(true);
   const [cardData, setCardData] = useState([]);
+
+  // MTG API url
+  const cardsUrl =
+    'https://api.magicthegathering.io/v1/cards?supertypes=legendary&types=creature';
 };
 
 export default CommanderContext;
