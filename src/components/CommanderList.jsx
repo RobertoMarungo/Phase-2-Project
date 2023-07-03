eadaf193178b9b8e49a9f32548eb0883414620eb
Roberto Marungo <robmarungo.dev@gmail.com>
2023-07-03 01:55:54 -0500

    Updated Commander List Component

--- src/components/CommanderList.jsx
+++ src/components/CommanderList.jsx
@@ -1,4 +1,23 @@
+import CommanderItem from './CommanderItem';
+import { useContext } from 'react';
+import CommanderContext from '../context/CommanderContext';
+import Spinner from './shared/Spinner';
+
 function CommanderList() {
-  return <div>CommanderList</div>;
+  const { cardData, isLoading } = useContext(CommanderContext);
+
+  return isLoading ? (
+    <Spinner />
+  ) : (
+    <div className='card-list'>
+      {cardData.map((card) => (
+        <CommanderItem
+          key={card.id}
+          card={card}
+        />
+      ))}
+    </div>
+  );
 }
+
 export default CommanderList;
