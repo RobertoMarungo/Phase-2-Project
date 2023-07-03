18b12480cc9f992d84586c5e62a54d9d9d3f96b2
Roberto Marungo <robmarungo.dev@gmail.com>
2023-07-03 01:57:19 -0500

    Updated App Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from './components/Header';
+import CommanderList from './components/CommanderList';
 
 function App() {
   return (
     <div className='App'>
       <Header />
+      <CommanderList />
     </div>
   );
 }
