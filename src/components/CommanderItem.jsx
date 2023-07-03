eb8e5e4f95918f9407dedcbd09ee82cff3814e8f
Roberto Marungo <robmarungo.dev@gmail.com>
2023-07-03 01:51:13 -0500

    Updated Commander Item Component

--- src/components/CommanderItem.jsx
+++ src/components/CommanderItem.jsx
@@ -1,4 +1,28 @@
-function CommanderItem() {
-  return <div>CommanderItem</div>;
+import Card from './shared/Card';
+
+function CommanderItem({ card }) {
+  return (
+    <Card>
+      <h2>
+        <strong>{card.name} 🦇</strong>
+      </h2>
+
+      <img
+        src={card.imageUrl || 'https://i.imgur.com/LdOBU1I.jpg'}
+        alt='mtg-card'></img>
+
+      <h4>
+        {card.rarity}{' '}
+        {card.legalities.map((legal) =>
+          legal.format === 'Commander' ? (
+            <p key={card.id}>
+              {legal.format} : {legal.legality}
+            </p>
+          ) : null
+        )}
+      </h4>
+      <p>{card.originalText}</p>
+    </Card>
+  );
 }
 export default CommanderItem;
