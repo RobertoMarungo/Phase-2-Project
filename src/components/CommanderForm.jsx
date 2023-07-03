3b2257845817ef675b821a12861273352af146fc
Roberto Marungo <robmarungo.dev@gmail.com>
2023-07-03 02:13:24 -0500

    Updated Commander Form Component

--- src/components/CommanderForm.jsx
+++ src/components/CommanderForm.jsx
@@ -1,4 +1,31 @@
 function CommanderForm() {
-  return <div>CommanderForm</div>;
+  return (
+    <div>
+      <form>
+        <input
+          type='text'
+          placeholder='Commander Name'
+        />
+        <br />
+        <label for='rarity'>Choose a Rarity: </label>
+        <select
+          name='rarity'
+          id='rarity'>
+          <option value='Common'>Common</option>
+          <option value='Uncommon'>Uncommon</option>
+          <option value='Rare'>Rare</option>
+          <option value='Mythic Rare'>Mythic Rare</option>
+        </select>
+        <br />
+        <label for='legality'>Choose a Legality: </label>
+        <select
+          name='legality'
+          id='legality'>
+          <option value='Common'>Legal</option>
+          <option value='Uncommon'>Banned</option>
+        </select>
+      </form>
+    </div>
+  );
 }
 export default CommanderForm;
